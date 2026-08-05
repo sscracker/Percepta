eb32dab703fa73b2ffe752455516e97e38b7256f
Vladimir Grigorev <60172225+sscracker@users.noreply.github.com>
2026-08-05 15:40:48 +0400

    [PER-006] - add detection for desktop

--- shared/src/jvmMain/kotlin/com/percepta/app/ImagePicker.jvm.kt
+++ shared/src/jvmMain/kotlin/com/percepta/app/ImagePicker.jvm.kt
@@ -2,13 +2,26 @@ package com.percepta.app
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.decodeToImageBitmap
+import javax.swing.JFileChooser
+import javax.swing.filechooser.FileNameExtensionFilter
 
 @Composable
 actual fun rememberImagePickerAction(onImagePicked: (ImageBitmap) -> Unit): () -> Unit {
-    TODO("Not yet implemented")
+    return {
+        val chooser = JFileChooser().apply {
+            fileFilter = FileNameExtensionFilter("Images", "jpg", "jpeg", "png")
+        }
+        val result = chooser.showOpenDialog(null)
+        if (result == JFileChooser.APPROVE_OPTION) {
+            val bytes = chooser.selectedFile.readBytes()
+            onImagePicked(bytes.decodeToImageBitmap())
+        }
+    }
 }
 
 @Composable
 actual fun rememberCameraCaptureAction(onImageCaptured: (ImageBitmap) -> Unit): () -> Unit {
-    TODO("Not yet implemented")
+    return  {
+    }
 }
